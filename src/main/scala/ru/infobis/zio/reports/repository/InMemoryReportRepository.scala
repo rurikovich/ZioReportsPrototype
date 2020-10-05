54688bdce3bf1856fb04d8df723e5a8db0f042ee
yuri <urrast@yandex.ru>
2020-10-05 18:10:44 +0400

    veryLongAndFatReportById

--- src/main/scala/ru/infobis/zio/reports/repository/InMemoryReportRepository.scala
+++ src/main/scala/ru/infobis/zio/reports/repository/InMemoryReportRepository.scala
@@ -1,21 +1,35 @@
 package ru.infobis.zio.reports.repository
 
+import java.time.LocalDateTime._
+
 import ru.infobis.zio.reports._
 import zio._
 
-final private class InMemoryReportRepository(ref: Ref[Map[Long, Report]])
+final private class InMemoryReportRepository()
   extends ReportsRepository.Service {
 
-  override def getById(id: Long): UIO[Option[Report]] = ref.get.map(_.get(id))
+  val requestDurationInSeconds = 20L
+
+  override def getById(id: Long): UIO[Option[Report]] = UIO.succeed(veryLongAndFatReportById(id, requestDurationInSeconds))
+
+  def veryLongAndFatReportById(id: Long, secondsToCalculateReport: Long): Option[Report] = {
+    val startTime = now()
+    val endTime = startTime.plusSeconds(secondsToCalculateReport)
+
+    var veryFatVal = (0 to 1000).foldLeft("")((res, i) => res + s"i=${i}_")
+    while (now() isBefore endTime) {
+      veryFatVal = veryFatVal + s"square=${Math.sqrt(111_231)}"
+    }
+
+    Some(Report(id, veryFatVal))
+  }
 
 }
 
 object InMemoryReportRepository {
 
   val layer: ZLayer[Any, Nothing, ReportsRepository] =
-    ZLayer.fromEffect {
-      for {
-        ref <- Ref.make(Map(10000L -> Report(10000, "eeee")))
-      } yield new InMemoryReportRepository(ref)
+    ZLayer.succeed {
+      new InMemoryReportRepository()
     }
 }
