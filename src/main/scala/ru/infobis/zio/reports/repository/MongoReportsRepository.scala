dd3d3394e4862d738c9db0e9d6f7bb23cc2f15b6
rurik <urrast@yandex.ru>
2020-10-21 11:38:22 +0400

    CommandListener

--- src/main/scala/ru/infobis/zio/reports/repository/MongoReportsRepository.scala
+++ src/main/scala/ru/infobis/zio/reports/repository/MongoReportsRepository.scala
@@ -7,7 +7,9 @@ import com.mongodb.BasicDBObject
 import com.mongodb.casbah.Imports._
 import com.mongodb.casbah.MongoDB
 import com.mongodb.casbah.query.Imports.{BasicDBList, MongoDBObject}
+import com.mongodb.event.{CommandFailedEvent, CommandListener, CommandStartedEvent, CommandSucceededEvent}
 import ru.infobis.zio.reports.Report
+import ru.infobis.zio.reports.repository.MongoReportsRepository.queries
 import salat.annotations._
 import salat.dao._
 import salat.global._
@@ -17,8 +19,31 @@ import zio.{RIO, UIO, ZLayer}
 
 case class Message(@Key("_id") id: Option[Long], sourceId: Long, mTime: Long, mType: Long, filterMessageType: Option[Long] = None)
 
+
 class MongoReportsRepository() extends {
-  private val client: MongoClient = MongoClient(host = "10.0.14.214")
+
+
+  val cl = new CommandListener() {
+    override def commandStarted(event: CommandStartedEvent): Unit = {
+      queries = queries ++ List(event.getCommand.get("query").toString)
+      println("")
+    }
+
+    override def commandSucceeded(event: CommandSucceededEvent): Unit = {
+      println("")
+    }
+
+    override def commandFailed(event: CommandFailedEvent): Unit = {
+      println("")
+    }
+
+  }
+
+
+  private val options = new MongoClientOptions.Builder().addCommandListener(cl).build()
+
+  private val client: MongoClient = MongoClient(host = "10.0.14.214", options = options)
+
 } with SalatDAO[Message, Long](collection = client("kdv")("messages"))
   with ReportsRepository.Service {
 
@@ -57,17 +82,31 @@ class MongoReportsRepository() extends {
       val ops: DBObject = dB.getCollection("$cmd.sys.inprog").findOne(MongoDBObject("$all" -> true))
       val activeOps: List[BasicDBObject] = getList[BasicDBObject](ops, "inprog").filter(_.getBoolean("active"))
 
-      val reportOps: List[BasicDBObject] = activeOps.filter(
-        _.getAs[BasicDBObject]("query").flatMap {
-          _.getAs[BasicDBObject]("query").map {
-            _.contains(reportId)
-          }
-        }.getOrElse(false)
+      //      val reportOps: List[BasicDBObject] = activeOps.filter(
+      //        _.getAs[BasicDBObject]("query").flatMap {
+      //          _.getAs[BasicDBObject]("query").map {
+      //            _.contains(reportId)
+      //          }
+      //        }.getOrElse(false)
+      //      )
+
+
+      val activQueries: List[BasicDBObject] = activeOps.flatMap(_.getAs[BasicDBObject]("query"))
+
+      activQueries.filter(
+        q => {
+          val maybeObject: Option[BasicDBObject] = q.getAs[BasicDBObject]("query")
+
+          maybeObject.forall((o: BasicDBObject) =>queries.contains(o.toString))
+        }
+      ).foreach(
+        op => killOp(dB, op.getInt("opid"))
       )
 
-      reportOps.foreach {
-        op => killOp(dB, op.getInt("opid"))
-      }
+
+      //        reportOps.foreach {
+      //        op => killOp(dB, op.getInt("opid"))
+      //      }
 
     }
   }
@@ -91,6 +130,8 @@ class MongoReportsRepository() extends {
 }
 
 object MongoReportsRepository {
+  var queries: List[String] = scala.List.empty[String]
+
   val layer: ZLayer[Any, Nothing, ReportsRepository] =
     ZLayer.succeed {
       new MongoReportsRepository()
